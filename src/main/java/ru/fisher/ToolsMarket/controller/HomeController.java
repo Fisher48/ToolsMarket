fa8a1c9973d9467e81d60b7a2b638732f7f24b53
fisher48 <rybakov48@yandex.ru>
2025-10-25 18:01:25 +0300

    Added MainPage Controller

---
+++ src/main/java/ru/fisher/ToolsMarket/controller/HomeController.java
@@ -0,0 +1,26 @@
+package ru.fisher.ToolsMarket.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.fisher.ToolsMarket.service.CategoryService;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        // Получаем ВСЕ категории для главной страницы
+        model.addAttribute("allCategories", categoryService.findAll());
+
+        // И корневые категории для меню навигации
+        model.addAttribute("categories", categoryService.getRootCategories());
+
+        return "index";
+    }
+
+}
