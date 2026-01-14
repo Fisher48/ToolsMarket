191f4fa1d5b1184568533b3d6399e7c90633af1a
fisher48 <rybakov48@yandex.ru>
2026-01-14 17:45:00 +0300

    Added display of Username in the icon

--- src/main/java/ru/fisher/ToolsMarket/controller/HomeController.java
+++ src/main/java/ru/fisher/ToolsMarket/controller/HomeController.java
@@ -1,16 +1,23 @@
 package ru.fisher.ToolsMarket.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.fisher.ToolsMarket.models.User;
 import ru.fisher.ToolsMarket.service.CategoryService;
+import ru.fisher.ToolsMarket.service.UserService;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final CategoryService categoryService;
+    private final UserService userService;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -19,6 +26,10 @@ public class HomeController {
 
         // И корневые категории для меню навигации
         model.addAttribute("categories", categoryService.getRootCategories());
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Optional<User> user = userService.findByUsername(auth.getName());
+        model.addAttribute("user", user);
 
         return "index";
     }
