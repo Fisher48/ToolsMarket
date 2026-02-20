e9ac63936fb7a113b6f69d0a14ef48356c86b8c6
fisher48 <rybakov48@yandex.ru>
2026-02-20 18:40:27 +0300

    Refactoring Cart logic (remove anonymous cart and session check)

--- src/main/java/ru/fisher/ToolsMarket/util/CartComponent.java
+++ src/main/java/ru/fisher/ToolsMarket/util/CartComponent.java
@@ -1,31 +1,31 @@
-package ru.fisher.ToolsMarket.util;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import ru.fisher.ToolsMarket.models.Cart;
-import ru.fisher.ToolsMarket.service.CartService;
-
-@ControllerAdvice
-public class CartComponent {
-
-    private final CartService cartService;
-
-    public CartComponent(CartService cartService) {
-        this.cartService = cartService;
-    }
-
-    @ModelAttribute("cartItemCount")
-    public int getCartItemCount(@CookieValue(value = "sessionId", required = false)
-                                String sessionId) {
-        if (sessionId == null) {
-            return 0;
-        }
-        try {
-            Cart cart = cartService.getOrCreateCart(null, sessionId);
-            return cartService.getCartItems(cart.getId()).size();
-        } catch (Exception e) {
-            return 0;
-        }
-    }
-}
+//package ru.fisher.ToolsMarket.util;
+//
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.CookieValue;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import ru.fisher.ToolsMarket.models.Cart;
+//import ru.fisher.ToolsMarket.service.CartService;
+//
+//@ControllerAdvice
+//public class CartComponent {
+//
+//    private final CartService cartService;
+//
+//    public CartComponent(CartService cartService) {
+//        this.cartService = cartService;
+//    }
+//
+//    @ModelAttribute("cartItemCount")
+//    public int getCartItemCount(@CookieValue(value = "sessionId", required = false)
+//                                String sessionId) {
+//        if (sessionId == null) {
+//            return 0;
+//        }
+//        try {
+//            Cart cart = cartService.getOrCreateCart(null, sessionId);
+//            return cartService.getCartItems(cart.getId()).size();
+//        } catch (Exception e) {
+//            return 0;
+//        }
+//    }
+//}
